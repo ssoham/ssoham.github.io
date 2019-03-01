85ad18dc882fb6de27018e7f2e3cd54090c705d2
whattrash <21ss0676@wwprsd.org>
2019-03-01 10:48:15 -0500

    github

--- particles/app.js
+++ particles/app.js
@@ -11,99 +11,99 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 /* Otherwise just put the config content (json): */
 
-particlesJS(
-  "particles-js",
-
+particlesJS('particles-js',
+  
   {
-    particles: {
-      number: {
-        value: 80,
-        density: {
-          enable: true,
-          value_area: 800
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
         }
       },
-      color: {
-        value: "#ffffff"
+      "color": {
+        "value": "#ffffff"
       },
-      shape: {
-        type: "circle",
-        stroke: {
-          width: 0,
-          color: "#000000"
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
         },
-        polygon: {
-          nb_sides: 5
+        "polygon": {
+          "nb_sides": 5
         },
-        image: {
-          src: "img/github.svg",
-          width: 100,
-          height: 100
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
         }
       },
-      opacity: {
-        value: 0.5,
-        random: false,
-        anim: {
-          enable: false,
-          speed: 1,
-          opacity_min: 0.1,
-          sync: false
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
         }
       },
-      size: {
-        value: 5,
-        random: true,
-        anim: {
-          enable: false,
-          speed: 40,
-          size_min: 0.1,
-          sync: false
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
         }
       },
-      line_linked: {
-        enable: true,
-        distance: 150,
-        color: "#ffffff",
-        opacity: 0.4,
-        width: 1
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
       },
-      move: {
-        enable: true,
-        speed: 6,
-        direction: "none",
-        random: false,
-        straight: false,
-        out_mode: "out",
-        attract: {
-          enable: false,
-          rotateX: 600,
-          rotateY: 1200
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
         }
       }
     },
-    interactivity: {
-      detect_on: "canvas",
-      events: {
-        onhover: {
-          enable: false,
-          mode: "repulse"
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
         },
-        onclick: {
-          enable: false,
-          mode: "push"
+        "onclick": {
+          "enable": false,
+          "mode": "push"
         },
-        resize: false
-      }
+        "resize": false
+      },
     },
-    retina_detect: true,
-    config_demo: {
-      hide_card: false,
-      background_color: "#b61924",
-      background_image: "",
-      background_position: "50% 50%",
-      background_repeat: "no-repeat",
-      background_size: "cover"
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
     }
   }
+
 );
